b0161048362cc39ace5b448c272d5e34fef34fe5
Patrick Müller <dev@mupat.net>
2014-06-10 10:07:32 +0200

    add default description for omnibox

--- js/background.js
+++ js/background.js
@@ -1,42 +1,46 @@
+'use strict';
+
 /* Bof: add trigger for omnibox search */
-  //on every new key stroke in the omnibox, search all bookmarks 
-  //and suggest this entries, where the name or the url matchs the search string
-  chrome.omnibox.onInputChanged.addListener(function(text, suggest) {
-    var suggestions = [];
-    var addEntry = function(item) {
-      var obj = {
-        content: item.url,
-        description: item.name
-      }
-      suggestions.push(obj);
+chrome.omnibox.setDefaultSuggestion({description: "name or url of your bookmark"})
+
+//on every new key stroke in the omnibox, search all bookmarks 
+//and suggest this entries, where the name or the url matchs the search string
+chrome.omnibox.onInputChanged.addListener(function(text, suggest) {
+  var suggestions = [];
+  var addEntry = function(item) {
+    var obj = {
+      content: item.url,
+      description: item.name
+    }
+    suggestions.push(obj);
+  }
+
+  for (var i = 0; i < localStorage.length; i++) {
+    var lsKey = localStorage.key(i); //get key from localstorage
+    //continue if the key isnt a item key
+    if(lsKey === "selection" || lsKey === "columns") continue;
+
+    //try to parse localstorage item to json obj
+    try {
+      item = JSON.parse(localStorage[lsKey]);
+    } catch (e) { //continue if it not works
+      continue;
     }
 
-    for (var i = 0; i < localStorage.length; i++) {
-      var lsKey = localStorage.key(i); //get key from localstorage
-      //continue if the key isnt a item key
-      if(lsKey === "selection" || lsKey === "columns") continue;
+    //check if input matches either name or url
+    if(item.name.indexOf(text.toLowerCase()) > -1) {
+      addEntry(item);
+    }else if(item.url.indexOf(text.toLowerCase()) > -1) {
+      addEntry(item)
+    }
+  };
 
-      //try to parse localstorage item to json obj
-      try {
-        item = JSON.parse(localStorage[lsKey]);
-      } catch (e) { //continue if it not works
-        continue;
-      }
+  //give items as suggestions back to chrome
+  suggest(suggestions);
+});
 
-      //check if input matches either name or url
-      if(item.name.indexOf(text.toLowerCase()) > -1) {
-        addEntry(item);
-      }else if(item.url.indexOf(text.toLowerCase()) > -1) {
-        addEntry(item)
-      }
-    };
-
-    //give items as suggestions back to chrome
-    suggest(suggestions);
-  });
-
-  //add listener to input entered event and open the given url in the current window
-  chrome.omnibox.onInputEntered.addListener(function(text) {
-    window.open(text);
-  });
+//add listener to input entered event and open the given url in the current window
+chrome.omnibox.onInputEntered.addListener(function(text) {
+  window.open(text);
+});
 /* ---- */
