5be140c5ba8f2274b461a751cf6733d202a6cf08
maccypher <maccypher@googlemail.com>
2014-12-04 08:05:59 +0100

    added linebreaks in html source code

--- client/scripts/main.coffee
+++ client/scripts/main.coffee
@@ -4,10 +4,18 @@ window.onload = ->
 		data = CKEDITOR.instances.editor1.getData()
 		docTitle = document.getElementById("docTitle").value + ".htm"
 
-		htmlString = window.headerText + data + window.footerText
-		document.getElementById("output").innerText = htmlString
+		dataString = data
+		re1 = /</gi
+		re2 = />/gi
+		dataString = dataString.replace re1, "\n    <"
+		dataString = dataString.replace re2, ">\n        "
+
+		htmlString = window.headerText + dataString + window.footerText
+
+		document.getElementById("output").value = htmlString
+		document.getElementById("output").classList.add('active')
 		blob = new Blob([htmlString], {type: "text/html;charset=utf-8"})
 
-		saveAs(blob, docTitle)
+		# saveAs(blob, docTitle)
 
 	document.getElementById("saveAs").addEventListener 'click', generate
--- client/vendor/footer.js
+++ client/vendor/footer.js
@@ -1,20 +1,21 @@
-window.footerText = '		<div class="bottom cf">' +
-'     <div class="copyright ui-droppable">' +
-'      <p class="paragraph1">Copyright © BlackBridge. All rights reserved.</p>' +
-'     </div>' +
-'    </div>' +
-'   </div>' +
-'   <div class="row_2_unit_wrapper cf"></div>' +
-'  </div>' +
-'<script type="text/javascript">' +
-'  var _gaq = _gaq || [];' +
-'  _gaq.push([\'_setAccount\', \'UA-46739050-1\']);' +
-'  _gaq.push([\'_trackPageview\']);' +
-'  (function() {' +
-'    var ga = document.createElement(\'script\'); ga.type = \'text/javascript\'; ga.async = true;' +
-'    ga.src = (\'https:\' == document.location.protocol ? \'https://ssl\' : \'http://www\') + \'.google-analytics.com/ga.js\';' +
-'    var s = document.getElementsByTagName(\'script\')[0]; s.parentNode.insertBefore(ga, s);' +
-'  })();' +
-'</script>' +
-' </body>' +
+window.footerText = "\n" + '    <!-- Generated Content ends here -->' + "\n" +
+'    <div class="bottom cf">' + "\n" + 
+'     <div class="copyright ui-droppable">' + "\n" + 
+'      <p class="paragraph1">Copyright &copy; BlackBridge. All rights reserved.</p>' + "\n" + 
+'     </div>' + "\n" + 
+'    </div>' + "\n" + 
+'   </div>' + "\n" + 
+'   <div class="row_2_unit_wrapper cf"></div>' + "\n" + 
+'  </div>' + "\n" + 
+'  <script type="text/javascript">' + "\n" + 
+'    var _gaq = _gaq || [];' + "\n" + 
+'    _gaq.push([\'_setAccount\', \'UA-46739050-1\']);' + "\n" + 
+'    _gaq.push([\'_trackPageview\']);' + "\n" + 
+'    (function() {' + "\n" + 
+'      var ga = document.createElement(\'script\'); ga.type = \'text/javascript\'; ga.async = true;' + "\n" + 
+'      ga.src = (\'https:\' == document.location.protocol ? \'https://ssl\' : \'http://www\') + \'.google-analytics.com/ga.js\';' + "\n" + 
+'      var s = document.getElementsByTagName(\'script\')[0]; s.parentNode.insertBefore(ga, s);' + "\n" + 
+'    })();' + "\n" + 
+'  </script>' + "\n" + 
+' </body>' + "\n" + 
 '</html>';
